27bd5f36b3f8706cc00baf14341d01c1d0aa17ac
Raphael <rptksmil@gmail.com>
2025-07-24 09:57:38 +0300

    starting base skeleton for the project

--- crud.c
+++ crud.c
@@ -32,7 +32,21 @@ char* read_line_from_file(const char* filename, int line_number)
             break;
         }
     }
-
     fclose(file);
     return line;
 }
+
+
+// get_files(file): r working
+//     will get the cmd input
+//     check if the names corespond to files(.as) in the directory
+//     read the files into list(all the files) of list(all the lines) of list(all the the words)
+//         first line in file is the name of the file
+//     ERROR handling dont stop the project
+//     return the phrases extracted 
+
+
+// save_file(file, file_name, ending): 
+//     save a file(list of lines of list of words) by name and ending(am/ob)
+
+
---
+++ string_handler.c
@@ -0,0 +1,24 @@
+// string_handler.c in charge of handling and manipulating strings
+
+
+
+
+
+
+
+
+
+
+
+
+// check_macro_end(line):
+//     get the line containig macro_end and check its leagal
+//     if yes return 0 otherwise return an apropriait error
+
+
+
+// is_lable(word):
+//     get a string
+//     return true if it can be a lable
+
+
